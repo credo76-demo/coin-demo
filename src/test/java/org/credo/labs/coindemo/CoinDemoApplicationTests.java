8a39f2078c788684883d255119d78ce5fba3b081
gitea-admin <weiping0201@gmail.com>
2024-07-12 14:56:49 +0800

    Initial commit

---
+++ src/test/java/org/credo/labs/coindemo/CoinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.credo.labs.coindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
